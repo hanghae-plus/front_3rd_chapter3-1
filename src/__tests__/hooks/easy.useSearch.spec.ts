c1f105f28f445efba2bada38040d61f6be6f0e88
hanjung <hanjung@linecorp.com>
2024-10-31 13:05:16 +0900

    test

--- src/__tests__/hooks/easy.useSearch.spec.ts
+++ src/__tests__/hooks/easy.useSearch.spec.ts
@@ -3,7 +3,9 @@ import { act, renderHook } from '@testing-library/react';
 import { useSearch } from '../../hooks/useSearch.ts';
 import { Event } from '../../types.ts';
 
-it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {});
+it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {
+  expect(1).toEqual(1);
+});
 
 it('검색어에 맞는 이벤트만 필터링해야 한다', () => {});
 
